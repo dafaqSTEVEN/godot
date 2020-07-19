0db683eb9a020450948dba75d60c632d04faf03e
dafaqSTEVEN <36439494+dafaqSTEVEN@users.noreply.github.com>
2020-07-19 23:48:43 +0800

    err commit to revert

--- space_invader_godot/bullet.gd
+++ space_invader_godot/bullet.gd
@@ -2,13 +2,15 @@ extends RigidBody2D
 export var speed  = 250
 var screen_size
 
-func _ready() -> void:
-	screen_size = get_tree().get_viewport_rect().size
+
 
 func _process(delta: float) -> void:
 	linear_velocity = Vector2(0,0)
-	
-	
 
-	
 
+
+
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
--- space_invader_godot/main.gd
+++ space_invader_godot/main.gd
@@ -9,10 +9,11 @@ func _ready() -> void:
 
 
 func _on_player_fire() -> void:
-	var obj = bullet.instance()
-	add_child(obj)
-	#reposition due to image difference.
-	obj.position = Vector2($player.position.x -10,$player.position.y - 15 )
-	obj.linear_velocity = Vector2(0,-1000)
+	if get_child_count() < 4:
+			var obj = bullet.instance()
+			add_child(obj)
+			#reposition due to image difference.
+			obj.position = Vector2($player.position.x -10,$player.position.y - 15 )
+			obj.linear_velocity = Vector2(0,-1000)
 	
 	
